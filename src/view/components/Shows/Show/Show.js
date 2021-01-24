c6890d317b8fa78b7a043da9bcc0beb4f633ec6d
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2021-01-24 21:40:01 +0100

    changing back color

--- src/view/components/Shows/Show/Show.js
+++ src/view/components/Shows/Show/Show.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import './Show.css';
+
 const Show = ({ image }) => {
     return(
-        <div className='col-3'>
+        <div className='col-3 show'>
             <img src={image} />
         </div>
     )
