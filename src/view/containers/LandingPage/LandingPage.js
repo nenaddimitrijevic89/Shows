601b335fa1c46b1990121c7de018f4093436fe9f
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2021-02-02 12:32:09 +0100

    fixing pagination

--- src/view/containers/LandingPage/LandingPage.js
+++ src/view/containers/LandingPage/LandingPage.js
@@ -3,22 +3,50 @@ import { connect } from "react-redux";
 
 import * as actions from "../../../store/action";
 import NavBar from "../../components/NavBar/NavBar";
+import Pagination from "../../components/Pagination/Pagination";
 import Shows from "../../components/Shows/Shows";
 
 class LandingPage extends Component {
+  state = {
+    showsPerPage: 20,
+    currentPage: 1,
+  };
   componentDidMount() {
     this.props.onFetchInitShows();
   }
 
+  paginate = (number) => {
+    const indexOfLastShow = this.state.currentPage * this.state.showsPerPage;
+    const indexOfFirstShow = indexOfLastShow - this.state.showsPerPage;
+    const currentShows = this.props.shows.slice(indexOfFirstShow, indexOfLastShow);
+    this.setState({ currentPage: number })
+    this.props.onChangePage(currentShows);
+  };
+
+  setShowsPerPage = (number) => {
+    this.setState({ showsPerPage: number });
+  };
+
   render() {
     let shows = null;
+    let pagination = null;
     if (this.props.shows) {
       shows = <Shows data={this.props.shows} />;
+      pagination = (
+        <Pagination
+          showsPerPage={this.state.showsPerPage}
+          totalShows={this.props.shows.length}
+          paginate={this.paginate}
+          currentPage={this.state.currentPage}
+          setShowsPerPage={this.setShowsPerPage}
+        />
+      );
     }
     return (
       <div className="container">
         <NavBar />
         {shows}
+        {pagination}
       </div>
     );
   }
@@ -33,6 +61,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     onFetchInitShows: () => dispatch(actions.fetchInitShows()),
+    onChangePage: (payload) => dispatch(actions.changePage(payload)),
   };
 };
 
