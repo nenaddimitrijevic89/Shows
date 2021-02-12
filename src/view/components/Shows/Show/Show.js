ca3ed115d6be6ece34ff3e3438b22237865fae40
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2021-02-13 00:05:59 +0100

    changing cols

--- src/view/components/Shows/Show/Show.js
+++ src/view/components/Shows/Show/Show.js
@@ -5,7 +5,7 @@ import './Show.css';
 const Show = ({ image }) => {
     // const pageNumbers = Array.from(new Array(pages)).map((_, index)=>index+1);
     return(
-        <div className='col- show'>
+        <div className='col-3 show'>
             <img src={image} alt='poster'/>
         </div>
     )
