2a3e2ec3f9c99b9ef3ad30026b9d886b1479423b
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2021-02-13 00:04:41 +0100

    changing

--- src/view/components/Shows/Show/Show.js
+++ src/view/components/Shows/Show/Show.js
@@ -5,7 +5,7 @@ import './Show.css';
 const Show = ({ image }) => {
     // const pageNumbers = Array.from(new Array(pages)).map((_, index)=>index+1);
     return(
-        <div className='col-3 show'>
+        <div className='col- show'>
             <img src={image} alt='poster'/>
         </div>
     )
