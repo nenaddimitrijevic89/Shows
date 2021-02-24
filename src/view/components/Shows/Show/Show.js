025248145082f74a9b68a69306ef791b5e56a6de
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2021-02-24 21:48:59 +0100

    changing show component

--- src/view/components/Shows/Show/Show.js
+++ src/view/components/Shows/Show/Show.js
@@ -5,10 +5,14 @@ import './Show.css';
 const Show = ({ image }) => {
     // const pageNumbers = Array.from(new Array(pages)).map((_, index)=>index+1);
     return(
-        <div className='col-lg-6 col-sm-12 show'>
+        <div className='col show'>
             <img src={image} alt='poster'/>
         </div>
     )
 };
 
 export default Show;
+
+{/* <div className='col-lg-6 col-sm-12 show'>
+            <img src={image} alt='poster'/>
+        </div> */}
