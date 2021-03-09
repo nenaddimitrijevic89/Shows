6e730a3a5f66f9849ddc8e2981fc09fee500adfd
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2021-03-09 22:15:20 +0100

    changing date format

--- src/view/components/Shows/SingleShowInfo/SingleShowInfo.js
+++ src/view/components/Shows/SingleShowInfo/SingleShowInfo.js
@@ -13,7 +13,7 @@ const SingleShowInfo = ({ image, name, summary, seasons }) => {
           <h2 className="text-center name">{name}</h2>
           <ul>
             <h5>Seasons ({seasons && seasons.length})</h5>
-            {seasons && seasons.map((season) => <li>{season.premiereDate}</li>)}
+            {seasons && seasons.map((season) => <li>{`${season.premiereDate} - ${season.endDate}`}</li>)}
           </ul>
         </div>
       </div>
