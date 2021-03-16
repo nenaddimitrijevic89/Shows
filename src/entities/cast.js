8574860cf41980dd7671f6b2d03d5704d6049f20
nenaddimitrijevic89 <nenaddimitrijevic89@gmail.com>
2021-03-16 22:32:18 +0100

    cast object changed

--- src/entities/cast.js
+++ src/entities/cast.js
@@ -2,7 +2,7 @@ class Cast {
   constructor(cast) {
     this.id = cast.character.id;
     this.name = cast.person.name;
-    this.image = cast.character.image ? cast.character.image.original : "avatar";
+    this.image = cast.character.image?.original || "avatar";
   }
 }
 
